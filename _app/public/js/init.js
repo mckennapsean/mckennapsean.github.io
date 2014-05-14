0280b471517dec45020c008cf40b2e3d144dbde9
Sean McKenna <mckennapsean@gmail.com>
2014-05-13 18:34:12 -0600

    add the initial prologue template

---
+++ _app/public/js/init.js
@@ -0,0 +1,58 @@
+/*
+  Prologue 1.0 by HTML5 UP
+  html5up.net | @n33co
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+/*********************************************************************************/
+/* Settings                                                                      */
+/*********************************************************************************/
+
+var prologue_settings = {
+
+  // skelJS (probably don't need to change anything here unless you know what you're doing)
+  skelJS: {
+    prefix: 'css/style',
+    resetCSS: true,
+    boxModel: 'border',
+    useOrientation: true,
+    breakpoints: {
+      'widest':  { range: '1881-', hasStyleSheet: false, containers: 1400, grid: { gutters: 70 } },
+      'wide':  { range: '961-1880', containers: 1200, grid: { gutters: 40 } },
+      'normal':  { range: '961-1620', containers: 960, grid: { gutters: 40 } },
+      'narrow':  { range: '961-1320', containers: 'fluid', grid: { gutters: 30 } },
+      'narrower':  { range: '-960', containers: 'fluid', grid: { gutters: 30 } },
+      'mobile':  { range: '-640', containers: 'fluid', lockViewport: true, grid: { gutters: 30, collapse: true } }
+    }
+  },
+
+  // skelJS Plugins (ditto; don't change unless you know what you're doing)
+  skelJSPlugins: {
+    panels: {
+      panels: {
+        sidePanel: {
+          breakpoints: 'narrower',
+          position: 'left',
+          size: 240,
+          html: '<div data-action="moveElement" data-args="header"></div></div>'
+        }
+      },
+      overlays: {
+        sidePanelToggle: {
+          breakpoints: 'narrower',
+          position: 'top-left',
+          width: '3.5em',
+          height: '2.25em',
+          html: '<div data-action="togglePanel" data-args="sidePanel" class="toggle"></div>'
+        }
+      }
+    }
+  }
+};
+
+/*********************************************************************************/
+/* Don't modify beyond this point unless you know what you're doing!             */
+/*********************************************************************************/
+
+// Initialize skelJS
+skel.init(prologue_settings.skelJS, prologue_settings.skelJSPlugins);
