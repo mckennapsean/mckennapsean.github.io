a1a6675d9c0eda8922651562336487768cc85d4b
Sean McKenna <mckennapsean@gmail.com>
2014-05-14 16:44:04 -0600

    update initial prologue settings from main template update

--- _app/public/js/init.js
+++ _app/public/js/init.js
@@ -17,12 +17,12 @@ var prologue_settings = {
     boxModel: 'border',
     useOrientation: true,
     breakpoints: {
-      'widest':  { range: '1881-', hasStyleSheet: false, containers: 1400, grid: { gutters: 70 } },
+      'widest':  { range: '1881-', hasStyleSheet: false, containers: 1400, grid: { gutters: 40 } },
       'wide':  { range: '961-1880', containers: 1200, grid: { gutters: 40 } },
       'normal':  { range: '961-1620', containers: 960, grid: { gutters: 40 } },
-      'narrow':  { range: '961-1320', containers: 'fluid', grid: { gutters: 30 } },
-      'narrower':  { range: '-960', containers: 'fluid', grid: { gutters: 30 } },
-      'mobile':  { range: '-640', containers: 'fluid', lockViewport: true, grid: { gutters: 30, collapse: true } }
+      'narrow':  { range: '961-1320', containers: 'fluid', grid: { gutters: 20 } },
+      'narrower':  { range: '-960', containers: 'fluid', grid: { gutters: 15 } },
+      'mobile':  { range: '-640', containers: 'fluid', lockViewport: true, grid: { gutters: 15, collapse: true } }
     }
   },
 
