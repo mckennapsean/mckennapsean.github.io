8c9b4bfee51c6ae7cbff8c24911adbd4345e1e96
Sean McKenna <mckennapsean@gmail.com>
2014-05-22 09:36:28 -0600

    update replace task to properly set all CDNs

--- _app/Gruntfile.js
+++ _app/Gruntfile.js
@@ -13,12 +13,27 @@ module.exports = function(grunt) {
     },
     replace: {
       dist: {
-        src: ['www/**/*.html'],
+        src: ['www/**/*.html', 'www/**/*.css'],
         overwrite: true,
         replacements: [
           {
-            from: '',
-            to: ''
+            from: '/vendor/jquery/dist/jquery.min.js',
+            to: '//cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js'
+          }, {
+            from: '/vendor/skeljs/dist/skel.min.js',
+            to: '//cdn.jsdelivr.net/skeljs/0.4.6/skel.min.js'
+          }, {
+            from: '/vendor/skeljs/dist/skel-panels.min.js',
+            to: '//cdn.jsdelivr.net/skeljs/0.4.6/skel-panels.min.js'
+          }, {
+            from: '/vendor/skeljs/dist/skel-noscript.css',
+            to: '//cdn.jsdelivr.net/skeljs/0.4.6/skel-noscript.css'
+          }, {
+            from: '/vendor/html5shiv/dist/html5shiv.min.js',
+            to: '//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.js'
+          }, {
+            from: '../vendor/font-awesome-bower/fonts/fontawesome-webfont',
+            to: '//cdn.jsdelivr.net/fontawesome/4.1.0/fonts/fontawesome-webfont'
           }
         ]
       }
