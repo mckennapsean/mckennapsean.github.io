504f10077e80ae394cab0aa3b9be72a7e9c9335f
Sean McKenna <mckennapsean@gmail.com>
2020-05-24 23:54:43 -0600

    [WIP] 11ty migration
    Not working: icons generally, project links

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+module.exports = function(eleventyConfig) {
+  // Aliases are in relation to the _includes folder
+  eleventyConfig.addLayoutAlias('project', 'layouts/project.liquid');
+  eleventyConfig.addLayoutAlias('site', 'layouts/site.liquid');
+
+  eleventyConfig.addCollection('projects', collection => {
+    return collection.getFilteredByGlob('_projects/*.md');
+  });
+  eleventyConfig.addCollection('coding', collection => {
+    return collection.getFilteredByGlob('_coding/*.md');
+  });
+  eleventyConfig.addCollection('games', collection => {
+    return collection.getFilteredByGlob('_games/*.md');
+  });
+
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('resources');
+  eleventyConfig.addPassthroughCopy('vendor');
+
+  return {
+    dir: {
+      includes: "_includes",
+      // layouts: "_layouts",
+      input: "./",
+      output: "./_site"
+    }
+  };
+};
